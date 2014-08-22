b2f35820677104117f3e1f4444a980131ebcc5b4
FunPanda <thisisspam1234567890@gmail.com>
2014-08-22 16:11:42 -0500

    New TMMaterials class

---
+++ src/main/java/com/funpanda/testmod/materials/TMMaterials.java
@@ -0,0 +1,13 @@
+package com.funpanda.testmod.materials;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MapColor;
+/**
+ * @author FunPanda
+ *         Thanks to Pahimar
+ *         Package: com.funpanda.testmod.materials
+ *         Date: 8/22/2014
+ */
+public class TMMaterials {
+    public static final Material test = new Material(MapColor.iceColor);
+}
