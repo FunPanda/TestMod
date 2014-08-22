a266da3c58d1ba444fb370f5dc1126b31aa8e28f
FunPanda <thisisspam1234567890@gmail.com>
2014-08-22 15:48:56 -0500

    Added right click potion effect to itemTest

--- src/main/java/com/funpanda/testmod/item/crafting/ItemTest.java
+++ src/main/java/com/funpanda/testmod/item/crafting/ItemTest.java
@@ -2,7 +2,11 @@ package com.funpanda.testmod.item.crafting;
 
 import com.funpanda.testmod.item.ItemTM;
 import com.funpanda.testmod.reference.ItemName;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
 
 /**
  * @author FunPanda
@@ -13,8 +17,21 @@ import net.minecraft.potion.Potion;
 public class ItemTest extends ItemTM{
        public ItemTest(){
            super();
-           this.setPotionEffect(Potion.regeneration.getName());
+           this.setPotionEffect("moveSpeed");
            this.setUnlocalizedName(ItemName.ITEMTEST_NAME);
+
+       }
+
+       public ItemStack onItemRightClick(ItemStack p_77659_1_, World p_77659_2_, EntityPlayer p_77659_3_){
+
+           if(p_77659_3_.getActivePotionEffect(Potion.regeneration)== null){
+               if(!p_77659_2_.isRemote){
+                   p_77659_3_.addPotionEffect(new PotionEffect(Potion.regeneration.id,300*20,5));
+               }
+
+           }
+
+           return p_77659_1_;
 
        }
 
